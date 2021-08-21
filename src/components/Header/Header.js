e3d5edbc6d33e257665df3c1fa7061945f0cdca8
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2021-08-21 17:14:42 +0300

    edit component Promo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Header.css";
 import Navigation from "../Navigation/Navigation";
-import Promo from "../Promo/Promo";
 
 export default function Header() {
   return (
     <>
       <header className="header">
         <Navigation place="header" />
-        <Promo />
       </header>
     </>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./Main.css";
+import Promo from "../Promo/Promo";
 import Navigation from "../Navigation/Navigation";
 import AboutProject from "../AboutProject/AboutProject";
 import Technology from "../Technology/Technology";
 import AboutMe from "../AboutMe/AboutMe";
 import Portfolio from "../Portfolio/Portfolio";
-import Footer from "../Footer/Footer";
 
 export default function Main() {
   return (
     <>
+      <Promo />
       <Navigation place="menu" />
       <AboutProject />
       <Technology />
       <AboutMe />
       <Portfolio />
-      <Footer />
     </>
   );
 }
