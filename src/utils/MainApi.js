5e24fd0fb48da83bddad1c02a6588a87ef4e8b20
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2021-08-26 11:03:53 +0300

    add and configure MainApi

---
+++ src/utils/MainApi.js
@@ -0,0 +1,44 @@
+const BASE_URL = "https://api.movies-star.nikolaym.nomoredomains.club";
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+const checkStatus = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка: ${res.status} ${res.statusText}`);
+};
+
+export const register = (name, email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ name, email, password }),
+  }).then((res) => {
+    return checkStatus(res);
+  });
+};
+
+export const login = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    return checkStatus(res);
+  });
+};
+
+export const getContent = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => {
+    return checkStatus(res);
+  });
+};
